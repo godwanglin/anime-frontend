6073b04d90e7ebd5053b78141b934765064f15be
fennai <fennai@local>
2026-04-25 18:25:33 +0700

    Make frontend deploy install resilient

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 set -e
 
-SCRIPT_PATH="$(readlink -f "$0")"
-SCRIPT_BACKUP="$(mktemp)"
-cp "$SCRIPT_PATH" "$SCRIPT_BACKUP"
-
 cd /home/www/app-anime/frontend-app
 
 echo "Syncing frontend with GitHub..."
@@ -12,13 +8,10 @@ git remote set-url origin https://github.com/xiaoycailin/anime-frontend.git
 git fetch origin
 git reset --hard origin/main
 git clean -fd
-cp "$SCRIPT_BACKUP" deploy.sh
-chmod +x deploy.sh
-rm -f "$SCRIPT_BACKUP"
 
 echo "Installing frontend dependencies..."
 if [ -f package-lock.json ]; then
-	npm ci --include=dev
+	npm ci --include=dev || npm install --include=dev
 else
 	npm install --include=dev
 fi

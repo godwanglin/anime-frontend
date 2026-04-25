b6b9ef846fb3f1bb6ea89780ca97c4a755fb2b92
fennai <fennai@local>
2026-04-26 04:54:19 +0700

    add put creds/me

--- src/routes/creds/me/+server.ts
+++ src/routes/creds/me/+server.ts
@@ -25,3 +25,29 @@ export const GET = async ({ request, cookies }) => {
 		status: 200
 	});
 };
+
+export const PUT = async ({ request, cookies }) => {
+	const baseUrl = config.API_BASE_URL;
+
+	const response = await fetch(`${baseUrl}/api/auth/me`, {
+		method: 'PUT',
+		headers: {
+			'Content-Type': 'application/json',
+			Authorization: `Bearer ${cookies.get('accessToken') ?? ''}`
+		},
+		body: await request.text()
+	});
+
+	if (!response.ok) {
+		const errorData = await response.json();
+		return new Response(JSON.stringify({ error: errorData.message || 'Login failed' }), {
+			status: response.status
+		});
+	}
+
+	const data = await response.json();
+
+	return new Response(JSON.stringify(data), {
+		status: 200
+	});
+};

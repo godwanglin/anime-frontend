4dedbdd52043ba9baba9419554666e6c87f786c4
fennai <fennai@local>
2026-04-29 06:10:42 +0700

    add client id

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -2,6 +2,8 @@ import { env } from '$env/dynamic/public';
 
 export default {
 	API_BASE_URL: env.PUBLIC_API_BASE_URL || 'https://api.weebin.site',
-	GOOGLE_CLIENT_ID: env.PUBLIC_GOOGLE_CLIENT_ID || '',
+	GOOGLE_CLIENT_ID:
+		env.PUBLIC_GOOGLE_CLIENT_ID ||
+		'199688498399-qa56cp7605ulssu93kmaleseq4pps4ra.apps.googleusercontent.com',
 	GOOGLE_REDIRECT_URI: env.PUBLIC_GOOGLE_REDIRECT_URI || 'https://weebin.site/creds/google/callback'
 };

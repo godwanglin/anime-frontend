1e5f69913762e77238d09b31988a8e99598c1933
fennai <fennai@local>
2026-08-27 03:52:21 +0700

    change base url

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,11 +1,11 @@
 import { env } from '$env/dynamic/public';
 
 export default {
-	API_BASE_URL: env.PUBLIC_API_BASE_URL || 'https://weebin.site',
+	API_BASE_URL: env.PUBLIC_API_BASE_URL || 'https://weebinhub.com',
 	// API_BASE_URL: env.PUBLIC_API_BASE_URL || 'http://localhost:3000',
 	GOOGLE_CLIENT_ID:
 		env.PUBLIC_GOOGLE_CLIENT_ID ||
 		'199688498399-qa56cp7605ulssu93kmaleseq4pps4ra.apps.googleusercontent.com',
-	GOOGLE_REDIRECT_URI: env.PUBLIC_GOOGLE_REDIRECT_URI || 'https://weebin.site/creds/google/callback',
+	GOOGLE_REDIRECT_URI: env.PUBLIC_GOOGLE_REDIRECT_URI || 'https://weebinhub.com/creds/google/callback',
 	ENABLE_PREMIUM_FEATURE: env.PUBLIC_ENABLE_PREMIUM_FEATURE === 'true'
 };
